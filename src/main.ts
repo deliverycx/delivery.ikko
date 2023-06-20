cbe5299c481db56fc2cd9e6dd7dff4b7d10d0196
xirurgx3d <xirurgx3d@gmail.com>
2023-06-20 14:18:41 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     },
   );
 	*/
-	/*
+	/* 
 	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
 	  transport: Transport.RMQ,
 	  options: {
