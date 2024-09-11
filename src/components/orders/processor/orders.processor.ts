4c920f1c6d89c7603b973b45acc5dc6b65988687
khinkalich <xirurgx3d@gmail.com>
2024-09-11 12:37:41 +0300

    q

--- src/components/orders/processor/orders.processor.ts
+++ src/components/orders/processor/orders.processor.ts
@@ -11,97 +11,97 @@ import { BotService } from '@app/shared/deliveryBot/bot.service';
 
 @Processor('order')
 export class OrdersConsumer {
-	
+
 	constructor(
 		@Inject(OrdersRepository) private readonly orderRepository,
 		private schedulerRegistry: SchedulerRegistry,
 		private readonly createOrderServise: CreateOrderServise,
 		private readonly botService: BotService
-	) {}
+	) { }
 
 
 
-  @Process('submit_order')
-  async transcode(job: Job<any>) {
-		
+	@Process('submit_order')
+	async transcode(job: Job<any>) {
+
 		const bodydata = {
 			"organizationId": job.data.organizationId,
 			"orderIds": [
 				job.data.orderIds
 			]
 		}
-		
+
 		const order = await this.createOrderServise.statusOrder(bodydata)
 
-		if(order.creationStatus){
-			console.log('старус оредера',order.creationStatus);
+		if (order.creationStatus) {
+			console.log('старус оредера', order.creationStatus);
 		}
 
-		
 
 
-		if(!order){
+
+		if (!order) {
 			const interval = this.schedulerRegistry.getInterval(job.data.hash as string);
-			
-			clearInterval(interval); 
+
+			clearInterval(interval);
 		}
-	
-		
 
-		if(order && order.creationStatus === 'Error'){
-			console.log('hash',job.data.hash);
+
+
+		if (order && order.creationStatus === 'Error') {
+			console.log('hash', job.data.hash);
 			const interval = this.schedulerRegistry.getInterval(job.data.hash as string);
-			clearInterval(interval); 
-			await this.orderRepository.orderUpdateBYID(order.id,{
-				orderStatus:"ERROR",
-				orderNumber:order.number,
-				orderError:order.errorInfo
+			clearInterval(interval);
+			await this.orderRepository.orderUpdateBYID(order.id, {
+				orderStatus: "ERROR",
+				orderNumber: order.number,
+				orderError: order.errorInfo
 			})
-			if(!order){
+			if (!order) {
 				const interval = this.schedulerRegistry.getInterval(job.data.hash as string);
-				clearInterval(interval); 
+				clearInterval(interval);
 				console.log('статуса нету');
 			}
 			console.log('ошибка в статусе');
-		} 
+		}
 
-		return {...order,hash:job.data.hash}
-  }
+		return { ...order, hash: job.data.hash }
+	}
 
 	@OnQueueActive()
-  async onActive(job: Job) { 
-		console.log('джоба');  
-	
-		
-  }
+	async onActive(job: Job) {
+		console.log('джоба');
 
 
-	@OnQueueCompleted() 
-	async complite(job: Job){
-		
-		if(job.returnvalue && job.returnvalue.creationStatus === 'Success'){
+	}
+
+
+	@OnQueueCompleted()
+	async complite(job: Job) {
+
+		if (job.returnvalue && job.returnvalue.creationStatus === 'Success') {
 			const order = job.returnvalue
-			console.log('order',order);
-			console.log('hash order ',order.hash);
+			console.log('order', order);
+			console.log('hash order ', order.hash);
 
 			const interval = this.schedulerRegistry.getInterval(order.hash as string);
-			
-			
-			clearInterval(interval); 
-			const resultOrder = await this.orderRepository.orderUpdateBYID(order.id,{
-				orderStatus:order.creationStatus,
-				orderNumber:order.order.number,
-				"orderParams.orderAmount":order.order.sum
+
+
+			clearInterval(interval);
+			const resultOrder = await this.orderRepository.orderUpdateBYID(order.id, {
+				orderStatus: order.creationStatus,
+				orderNumber: order.order.number,
+				"orderParams.orderAmount": order.order.sum
 			})
 			await this.botService.sendDuplicate(resultOrder)
-			
-		} 
-		
+
+		}
+
 		console.log('OnQueueCompleted закончилась');
 	}
 
 	@OnQueueStalled()
-	stalled(job: Job){
+	stalled(job: Job) {
 		/*
 		const order = job.returnvalue
 		const interval = this.schedulerRegistry.getInterval(job.id as string);
@@ -109,25 +109,27 @@ export class OrdersConsumer {
 		*/
 	}
 
-	 @OnQueueFailed()
-	 async falis(job: Job,err:Error){
-			const order = job.returnvalue
-		
+	@OnQueueFailed()
+	async falis(job: Job, err: Error) {
+		const order = job.returnvalue
+		if (order && order.hash) {
 			const interval = this.schedulerRegistry.getInterval(order.hash as string);
-			clearInterval(interval); 
-			await this.orderRepository.orderUpdateBYID(order.id,{
-				orderStatus:order.creationStatus,
-				orderError:err.message
+			clearInterval(interval);
+			await this.orderRepository.orderUpdateBYID(order.id, {
+				orderStatus: order.creationStatus,
+				orderError: err.message
 			})
-			
-		console.log('сломалось ',err.message);
+		}
+
+
+		console.log('сломалось ', err.message);
 	}
 
-	
+
 
 	@OnQueueError()
-	error(job: Job){
-	
+	error(job: Job) {
+
 		//const interval = this.schedulerRegistry.getInterval(job.id as string);
 		//clearInterval(interval);
 	}
